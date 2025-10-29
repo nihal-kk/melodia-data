7616ee50a2009641c889444af37bb9b03236ba82
nihal-kk <muhammadnihal4662@gmail.com>
2025-10-29 18:13:48 +0530

    Deploy simple JSON Server backend for Melodia

--- server.js
+++ server.js
@@ -17,15 +17,19 @@
 
 // server.js
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <- your db.json file
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 5000;
-
+server.use(cors());
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`🎶 Melodia JSON Server running on port ${PORT}`);
 });
+
